82bc928fb6e6c51cbfc1d412a2f553536179e5e0
Martin-Webdevmk <martin@webdevmk.com>
2025-04-23 00:42:00 +0200

    Added Google Map when login

--- lib/global/global_var.dart
+++ lib/global/global_var.dart
@@ -1,2 +1,9 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 String userName = "";
 String googleMapKey = "AIzaSyBL30OS_e4jblxAPO4fUWzI5LL5lk3O2uU";
+
+const CameraPosition googlePlexInitialPosition = CameraPosition(
+  target: LatLng(37.42796133580664, -122.085749655962),
+  zoom: 14.4746,
+);
