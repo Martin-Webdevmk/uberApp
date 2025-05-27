215ee03d0171094fe405bf7977671cf113db1537
Martin-Webdevmk <martin@webdevmk.com>
2025-05-27 17:30:05 +0200

    last
    last

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,6 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:users_app/global/global_var.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,29 +9,40 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-
-
-
 class _HomePageState extends State<HomePage> {
-
-  final Completer<GoogleMapController> googleMapCompleterController = Completer<GoogleMapController>();
-  GoogleMapController? controllerGoogleMap;
+  final mapCenter = LatLng(41.7961, 20.9039); // Gostivar, Macedonia
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Stack(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('OpenStreetMap Example'),
+      ),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: mapCenter,
+          initialZoom: 13,
+        ),
         children: [
-          GoogleMap(
-            mapType: MapType.satellite,
-            myLocationEnabled: true,
-            initialCameraPosition: googlePlexInitialPosition,
-            onMapCreated: (GoogleMapController mapController)
-            {
-              controllerGoogleMap = mapController;
-
-              googleMapCompleterController.complete(controllerGoogleMap);
-            },
+          TileLayer(
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            subdomains: const ['a', 'b', 'c'],
+            userAgentPackageName: 'com.martinaleksoski.usersapp',
+          ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: mapCenter,
+                width: 80,
+                height: 80,
+                alignment: Alignment.center,
+                child: const Icon(
+                  Icons.location_pin,
+                  size: 40,
+                  color: Colors.red,
+                ),
+              ),
+            ],
           ),
         ],
       ),
